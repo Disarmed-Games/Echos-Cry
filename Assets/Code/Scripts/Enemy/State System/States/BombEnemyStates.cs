fb14c68190611168953a531025b5fe175e242a43
Colby Gram <cgram79@yahoo.com>
2026-03-21 12:02:53 -0700

    working on states
    added bomb enemy states

---
+++ Assets/Code/Scripts/Enemy/State System/States/BombEnemyStates.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class BombEnemyStates : MonoBehaviour
+{
+    
+}
+
+public class BombSpawnState : EnemyState
+{
+    public BombSpawnState(Enemy enemyContext) : base(enemyContext)
+    {
+    }
+}
