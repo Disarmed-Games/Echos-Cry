9d1a367a3e99316b4ac8a817948d5f9a4e3c81f5
andy-wittig <157990692+andy-wittig@users.noreply.github.com>
2026-04-07 16:53:52 -0700

    Items can be dropped by hovering over and pressing 'q'.

--- Assets/Code/Scripts/UI/Inventory/slotScript.cs
+++ Assets/Code/Scripts/UI/Inventory/slotScript.cs
@@ -10,7 +10,12 @@ public class SlotScript : MonoBehaviour, IPointerEnterHandler, IPointerExitHandl
     [SerializeField] private TextMeshProUGUI stackAmountText;
     [SerializeField] private TextMeshProUGUI keyTooltipText;
     [SerializeField] private InputActionReference useItemInput;
+    [SerializeField] private InputTranslator inputTranslator;
+    [SerializeField] private GameObject droppedItem;
+
     private string description;
+    private InventoryItem slotItem;
+    private bool canDrop = false;
 
     private void Start()
     {
@@ -18,8 +23,29 @@ public class SlotScript : MonoBehaviour, IPointerEnterHandler, IPointerExitHandl
         keyTooltipText.text = useItemInput.action.GetBindingDisplayString(InputBinding.MaskByGroup("KeyboardMouse"));
     }
 
+    private void OnEnable()
+    {
+        inputTranslator.OnDropEvent += DropItem;
+    }
+    private void OnDisable()
+    {
+        inputTranslator.OnDropEvent -= DropItem;
+    }
+
+    private void DropItem()
+    {
+        if (canDrop && slotItem != null)
+        {
+            droppedItem.GetComponent<DroppedItem>().item = slotItem.data;
+            Instantiate(droppedItem, PlayerRef.Transform.position + new Vector3(0, 0, -1f), Quaternion.identity);
+            InventoryManager.Instance.Remove(slotItem.data);
+        }
+    }
+
     public void Set(InventoryItem item)
     {
+        slotItem = item;
+
         if (item == null || item.stackSize < 1)
         {
             slotIcon.enabled = false;
@@ -37,14 +63,19 @@ public class SlotScript : MonoBehaviour, IPointerEnterHandler, IPointerExitHandl
 
     public void OnPointerEnter(PointerEventData eventData)
     {
+        canDrop = true;
+
         if (description != "")
         {
-            UITip.Instance.StartMessage(description);
+            string dropText = $"\nPress '{inputTranslator.PlayerInputs.Gameplay.Drop.GetBindingDisplayString(InputBinding.MaskByGroup("KeyboardMouse"))}' to drop.";
+            UITip.Instance.StartMessage(description + dropText);
         }
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
+        canDrop = false;
+
         UITip.Instance.StopMessage();
     }
 }
