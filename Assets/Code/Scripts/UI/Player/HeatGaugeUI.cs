cf36a651dd4a3ae40a4271d42fb0e4b1c1749490
andy-wittig <157990692+andy-wittig@users.noreply.github.com>
2026-04-11 17:30:44 -0700

    Added use gauge sfx.

--- Assets/Code/Scripts/UI/Player/HeatGaugeUI.cs
+++ Assets/Code/Scripts/UI/Player/HeatGaugeUI.cs
@@ -1,10 +1,14 @@
 using UnityEngine;
 using UnityEngine.UI;
+using AudioSystem;
 
 public class HeatGaugeUI : MonoBehaviour
 {
     [SerializeField] DoubleIntEventChannel _updateHeatGauge;
     [SerializeField] Image[] heatImagesArray = new Image[6];
+    [SerializeField] private soundEffect useGaugeSFX;
+    private int pastGuageValue;
+
     private void OnEnable()
     {
         if (_updateHeatGauge != null) _updateHeatGauge.Channel += UpdateHeatGauge;
@@ -21,5 +25,15 @@ public class HeatGaugeUI : MonoBehaviour
         {
             heatImagesArray[i].enabled = (i < current);
         }
+
+        if (current < pastGuageValue)
+        {
+            SoundEffectManager.Instance.Builder
+            .SetSound(useGaugeSFX)
+            .SetSoundPosition(transform.position)
+            .ValidateAndPlaySound();
+        }
+
+        if (current != pastGuageValue) pastGuageValue = current;
     }
 }
