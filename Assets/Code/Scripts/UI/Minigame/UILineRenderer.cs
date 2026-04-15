539287528a5106b2ff273dec92766566caab55fd
andy-wittig <157990692+andy-wittig@users.noreply.github.com>
2026-04-14 19:01:29 -0700

    Improved heat gauge visuals.

--- Assets/Code/Scripts/UI/Minigame/UILineRenderer.cs
+++ Assets/Code/Scripts/UI/Minigame/UILineRenderer.cs
@@ -12,7 +12,6 @@ public class UILineRenderer : Graphic
     [SerializeField] private float lineWidth = 10f;
     [SerializeField] private Color onBeatLineColor;
     [SerializeField] private Color secondBeatLineColor;
-    [SerializeField] private Color fourthBeatLineColor;
 
     [Header("Gradient")]
     [SerializeField] private Color leftColor = Color.white;
@@ -84,10 +83,10 @@ public class UILineRenderer : Graphic
 
             if (BeatManager.Instance)
             {
-                if(BeatManager.Instance.BeatInMeasure == 1 && Player.Instance.HeatGauge.CurrentCharge >= 6)
+                if ((BeatManager.Instance.BeatInMeasure == 1 
+                    || BeatManager.Instance.BeatInMeasure == 3) 
+                    && Player.Instance.HeatGauge.CurrentCharge >= 6)
                     vertex.color = secondBeatLineColor;
-                else if (BeatManager.Instance.BeatInMeasure == 3 && Player.Instance.HeatGauge.CurrentCharge >= 6)
-                    vertex.color = fourthBeatLineColor;
                 else
                     vertex.color = onBeatLineColor;
             }
