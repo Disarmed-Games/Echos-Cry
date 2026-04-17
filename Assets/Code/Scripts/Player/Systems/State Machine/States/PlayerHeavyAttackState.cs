b297f1e7a5dc93e055f1fc279d9bbca170b2fc88
andy-wittig <157990692+andy-wittig@users.noreply.github.com>
2026-04-16 21:25:19 -0700

    Added sauce back to the game!

--- Assets/Code/Scripts/Player/Systems/State Machine/States/PlayerHeavyAttackState.cs
+++ Assets/Code/Scripts/Player/Systems/State Machine/States/PlayerHeavyAttackState.cs
@@ -11,7 +11,7 @@ public class PlayerHeavyAttackState : PlayerActionState
 
         _playerContext.Animator.SpriteAnimator.Play("Attack");
         CameraManager.Instance.ScreenShake(0.5f, 0.5f);
-        //_playerContext.Movement.MomentumPush();
+        _playerContext.Movement.MomentumPush();
         _playerContext.Orientation.IsRotating = false;
     }
     public override void Exit()
