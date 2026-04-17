16c15278b12863db1dc2f4494a7e9fdebce46c28
Abb6781 <151780295+Abb6781@users.noreply.github.com>
2026-04-16 23:57:08 -0700

    cutscene classes

---
+++ Assets/Level/Prefabs/UI/Cutscenes/CutsceneController.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+
+public class CutsceneController : MonoBehaviour{
+    private List<CutsceneEvent> events = new List<CutsceneEvent>();
+    private float currentTime = 0f;
+    private bool playing = false;
+
+    public void Play(List<CutsceneEvent> newEvents){
+        events = newEvents;
+        currentTime = 0f;
+        playing = true;
+    }
+    IEnumerator PlayCutscene(List<CutsceneEvent> events){
+        float time = 0f;
+
+        while (true){
+            time += Time.deltaTime;
+            foreach (var e in events){
+                if (!e.triggered && time >= e.triggerTime){
+                    e.Execute();
+                    e.triggered = true;
+                }if (time >= e.triggerTime){
+                    e.UpdateEvent(time);
+                }
+            }
+            yield return null;
+        }
+    }
+}
