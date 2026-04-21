9921c40a1fb122ae8ed233a3ae8d4c13b6b317b6
andy-wittig <157990692+andy-wittig@users.noreply.github.com>
2026-04-21 00:56:28 -0700

    Quick UI fix.

--- Assets/Code/Scripts/UI/Player/CurrencyCounterUI.cs
+++ Assets/Code/Scripts/UI/Player/CurrencyCounterUI.cs
@@ -5,13 +5,13 @@ public class CurrencyCounterUI : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI currencyText;
     [SerializeField] private Transform currencyTransform;
-    private float transitionTime = 0.10f;
+    private float transitionTime = 0.15f;
 
     public void UpdateCurrencyText(int currencyCount)
     {
         currencyTransform.DOKill();
         currencyTransform.localRotation = Quaternion.identity;
-        currencyTransform.DOShakeRotation(transitionTime, 45, 5, 45, true);
+        currencyTransform.DOShakeRotation(transitionTime, 20, 5, 45, true);
         currencyText.text = "Gold: " + currencyCount.ToString();
     }
 
