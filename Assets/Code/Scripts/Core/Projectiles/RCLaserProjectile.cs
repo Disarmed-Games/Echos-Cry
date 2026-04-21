d25783e02ac6aaa10c7b73f8989406e87bad2cd8
andy-wittig <157990692+andy-wittig@users.noreply.github.com>
2026-04-20 22:48:46 -0700

    Teleport enemy finished!

--- Assets/Code/Scripts/Core/Projectiles/RCLaserProjectile.cs
+++ Assets/Code/Scripts/Core/Projectiles/RCLaserProjectile.cs
@@ -9,9 +9,12 @@ public class RCLaserProjectile : AttackMethod
     [SerializeField] private LayerMask layerMask;
     [SerializeField] private float maxRayLength;
     [SerializeField] private float raySpeed;
+    [SerializeField] private float targetLerpSpeed = 0.5f;
+    [SerializeField] private float _duration;
     [SerializeField] private GameObject laserPrefab;
 
     private GameObject currentLaser;
+    private Vector3 smoothedTarget;
     private float rayLength;
     private float projectileDamage;
 
@@ -20,7 +23,10 @@ public class RCLaserProjectile : AttackMethod
         projectileDamage = damage;
         _attackEnded = false;
         currentLaser = Instantiate(laserPrefab, transform);
+        smoothedTarget = PlayerRef.Transform.position;
+
         StartCoroutine(StartAttack());
+        StartCoroutine(AttackDuration(_duration));
     }
 
     private IEnumerator StartAttack()
@@ -30,7 +36,13 @@ public class RCLaserProjectile : AttackMethod
             rayLength += raySpeed * Time.deltaTime;
             rayLength = Mathf.Min(rayLength, maxRayLength);
 
-            Vector3 direction = (PlayerRef.Transform.position - transform.position).normalized;
+            smoothedTarget = Vector3.Lerp(
+                smoothedTarget,
+                PlayerRef.Transform.position,
+                targetLerpSpeed * Time.deltaTime
+            );
+
+            Vector3 direction = (smoothedTarget - transform.position).normalized;
             direction.y = 0f;
 
             transform.rotation = Quaternion.LookRotation(direction, Vector3.up);
@@ -41,14 +53,26 @@ public class RCLaserProjectile : AttackMethod
             if (Physics.Raycast(transform.position, direction, out hit, rayLength, layerMask))
             {
                 Damage(hit.collider);
-                rayLength = 0;
-                _attackEnded = true;
-                Destroy(currentLaser);
+                AttackFinished();
                 break;
             }
             else yield return null;
         } 
     }
+
+    private IEnumerator AttackDuration(float dur)
+    {
+        yield return new WaitForSeconds(dur);
+        AttackFinished();
+    }
+
+    private void AttackFinished()
+    {
+        rayLength = 0;
+        _attackEnded = true;
+        Destroy(currentLaser);
+    }
+
     private void Damage(Collider collider)
     {
         AttackInfo attack = new AttackInfo.Builder().SetDamage(projectileDamage).Build();
