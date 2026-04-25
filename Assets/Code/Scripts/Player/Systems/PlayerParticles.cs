895331a527a9730bfa5709ca07b635367c06d69d
andy-wittig <157990692+andy-wittig@users.noreply.github.com>
2026-04-25 16:30:34 -0700

    Added sauce to player for note particles.

--- Assets/Code/Scripts/Player/Systems/PlayerParticles.cs
+++ Assets/Code/Scripts/Player/Systems/PlayerParticles.cs
@@ -5,19 +5,32 @@ public class PlayerParticles : MonoBehaviour
     //Particles
     [SerializeField] private ParticleSystem _experienceParticles;
     [SerializeField] private ParticleSystem _dashParticles;
+    [SerializeField] private ParticleSystem _noteHitParicles;
 
     //Channels
     [SerializeField] private IntEventChannel _levelUpChannel;
+    [SerializeField] private InputTranslator _inputTranslator;
 
     private void OnEnable()
     {
         _levelUpChannel.Channel += StartExperienceParticles;
+        _inputTranslator.OnPrimaryActionEvent += StartHitNoteParticles;
+        _inputTranslator.OnSecondaryActionEvent += StartHitNoteParticles;
+        _inputTranslator.OnSpecialAttackEvent += StartHitNoteParticles;
     }
     private void OnDisable()
     {
         _levelUpChannel.Channel -= StartExperienceParticles;
+        _inputTranslator.OnPrimaryActionEvent -= StartHitNoteParticles;
+        _inputTranslator.OnSecondaryActionEvent -= StartHitNoteParticles;
+        _inputTranslator.OnSpecialAttackEvent -= StartHitNoteParticles;
     }
 
+    private void StartHitNoteParticles(bool isPressed)
+    {
+        if (!isPressed) return;
+        _noteHitParicles.Play();
+    }
     private void StartExperienceParticles(int level)
     {
         _experienceParticles.Play();
