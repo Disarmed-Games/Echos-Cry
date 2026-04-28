c7068bfcb81d9c03eba5212fa84e3ebc23ca523c
andy-wittig <157990692+andy-wittig@users.noreply.github.com>
2026-04-27 20:34:32 -0700

    Fixed player progress bars.

--- Assets/Code/Scripts/UI/Player/PlayerProgressBar.cs
+++ Assets/Code/Scripts/UI/Player/PlayerProgressBar.cs
@@ -19,8 +19,11 @@ public class PlayerProgressBar : MonoBehaviour
     [SerializeField] private TextMeshProUGUI livesRemainingText;
     [SerializeField] private UnityEngine.UI.Image frontBar;
     [SerializeField] private UnityEngine.UI.Image backBar;
-    public float chipSpeed = 2f;
-    private float targetFraction;
+
+    public float lerpSpeed = 0.5f;
+    private float frontVelocity;
+    private float backVelocity;
+    private float targetFraction = 1f;
     private HealthSystem playerHealth = null;
 
     void OnEnable()
@@ -30,31 +33,19 @@ public class PlayerProgressBar : MonoBehaviour
 
         if (barType == Bar.HEALTH)
         {
-            //use stored reference rather than finding the object every time on death reset 
             if (playerHealth == null)
-            {
                 playerHealth = FindAnyObjectByType<HealthSystem>();
-            }
             
-            if (playerHealth != null) {
-                
-                //Debug.Log($"in progress bar curr health: {playerHealth.CurrentHealth}, max plyr health: {playerHealth.MaxHealth}");
+            if (playerHealth != null)
                 UpdateBar(playerHealth.CurrentHealth, playerHealth.MaxHealth);
-            }
         }
         else if (barType == Bar.ARMOR)
         {
             if (playerHealth == null)
-            {
                 playerHealth = FindAnyObjectByType<HealthSystem>();
-            }
 
-            if (playerHealth != null)
-            {
-                
-                //Debug.Log($"in progress bar curr armor: {playerHealth.CurrentArmor}, max plyr armor: {playerHealth.MaxArmor}");
-                UpdateBar(playerHealth.CurrentArmor, playerHealth.MaxArmor);
-            }    
+            if (playerHealth != null)  
+                UpdateBar(playerHealth.CurrentArmor, playerHealth.MaxArmor);  
         }
 
         UpdateLives();
@@ -78,31 +69,23 @@ public class PlayerProgressBar : MonoBehaviour
 
     private void UpdateBar(float currentValue, float maxValue)
     {
-        //Debug.Log($"Current value: {currentValue}, maxValue: {maxValue}");
         amountText.text = $"{currentValue}/{maxValue}";
-
         targetFraction = currentValue / maxValue;
+    }
+    void Update()
+    {
+        frontBar.fillAmount = Mathf.SmoothDamp(
+            frontBar.fillAmount,
+            targetFraction,
+            ref frontVelocity,
+            lerpSpeed
+        );
 
-        backBar.DOKill();
-        frontBar.DOKill();
-
-        float frontFill = frontBar.fillAmount;
-        float backFill = backBar.fillAmount;
-
-        if (backFill > targetFraction)
-        {
-            frontBar.fillAmount = targetFraction;
-            DOTween.To(() => backBar.fillAmount, x => backBar.fillAmount = x, targetFraction, chipSpeed).SetEase(Ease.OutQuad);
-        }
-        else if (frontFill < targetFraction)
-        {
-            backBar.fillAmount = targetFraction;
-            DOTween.To(() => frontBar.fillAmount, x => frontBar.fillAmount = x, targetFraction, chipSpeed).SetEase(Ease.OutQuad);
-        }
-        else
-        {
-            frontBar.fillAmount = targetFraction;
-            backBar.fillAmount = targetFraction;
-        }
+        backBar.fillAmount = Mathf.SmoothDamp(
+            backBar.fillAmount,
+            targetFraction,
+            ref backVelocity,
+            lerpSpeed * 1.5f
+        );
     }
 }
